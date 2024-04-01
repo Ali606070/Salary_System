346fdb89034c5cf3d720f27f5f024b2ce9918724
ali asswad <aliasswadali@gmail.com>
2024-03-31 18:57:21 -0700

    Add value object base class

---
+++ Domain/Primitives/ValueObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Primitives
+{
+    public abstract class ValueObject:IEquatable<ValueObject>
+    {
+        public abstract IEnumerable<object> GetAtomicValue();
+        public override bool Equals(object? obj)
+        {
+            return obj is ValueObject other && ValueAreEqual(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetAtomicValue()
+                .Aggregate(
+                    default(int),HashCode.Combine
+                );
+        }
+        private bool ValueAreEqual(ValueObject other)
+        {
+            return GetAtomicValue().SequenceEqual(other.GetAtomicValue());
+        }
+
+        public bool Equals(ValueObject? other)
+        {
+           return other is not null && ValueAreEqual(other);
+        }
+
+        public static bool operator ==(ValueObject? first, ValueObject? second)
+        {
+            return first is not null && second is not null && first.Equals(second);
+        }
+        public static bool operator !=(ValueObject? first, ValueObject? second)
+        {
+            return !(first == second);
+        }
+    }
+}
